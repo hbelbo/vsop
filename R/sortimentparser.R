d457b988f2693360bc1ad23e5b2a2fdfcd5a600d
Helmer Belbo <Helmer.Belbo@nibio.no>
2025-04-02 08:19:37 +0200

    sortimentparser for vsop data

--- R/sortimentparser.R
+++ R/sortimentparser.R
@@ -35,8 +35,60 @@ sortimentparser <- function(tbbl){
       assortment = dplyr::case_when(grovsortiment == "Skurtømmer" ~ "Sawlogs",
                              grovsortiment == "Massevirke" ~ "Pulpwood",
                              grovsortiment == "Sams" ~ "Mix",
-                             grovsortiment ==  "Ved til brensel bar"~ "Conifer Woodfuel",
+                             grovsortiment ==  "Ved til brensel bar"~ "Woodmix",
                              TRUE ~ sortiment)
+    )
+  return(tbbl)
+}
+
+
+
+
+
+#' Sortimentparser_vsop
+#'
+#' @param tbbl a tibble having the variables "sortiment" and "
+#'
+#' @returns a tibble
+#' @export
+#'
+#' @examples
+#' sortimentparser_vsop(avvirk_kmn_ldir_regkorigert)
+sortimentparser_vsop <- function(tbbl){
+  tbbl <- tbbl %>% dplyr::mutate(
+    trsl = tolower(virkesgrp),
+    treslag = dplyr::case_when(
+      stringr::str_detect(trsl, "gran") ~ "Gran",
+      stringr::str_detect(trsl, "furu") ~ "Furu",
+      stringr::str_detect(trsl, "lauv") ~ "Lauv",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Lauv ved ") ~ "Lauv",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Rundvirke til flis - lauv") ~ "Lauv",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Bar ved ") ~ "Bar",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Rundvirke til flis - bar") ~ "Bar",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Alle treslag ") ~ "Vedmix",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Heltreflis, lauv/bar" ) ~ "Flismiks",
+      stringr::str_detect(trsl, "ved") & stringr::str_detect(kategoritekst, "^Grotflis") ~ "Flismiks",
+      TRUE ~ "Annet"),
+    grovsortiment =  dplyr::case_when(
+    stringr::str_detect(sortiment, "Sagtømmer") ~ "Skurtømmer",
+    stringr::str_detect(sortiment, "Massevirke") ~ "Massevirke",
+    stringr::str_detect(sortiment, "Vrak") ~ "Vrak",
+    stringr::str_detect(sortiment, "Energi") ~ "Energi",
+    TRUE ~ sortiment))   %>%
+    dplyr::mutate(
+      species = dplyr::case_when(treslag == "Gran" ~ "Spruce",
+                                 treslag == "Furu" ~ "Pine",
+                                 treslag == "Lauv" ~ "Broadleave",
+                                 treslag == "Bar" ~ "Conifer",
+                                 treslag == "Vedmix" ~ "Woodmix",
+                                 treslag == "Flismix" ~ "Forest chips mix",
+                                 treslag == "Annet" ~ "Other",
+                                 TRUE ~ treslag),
+      assortment = dplyr::case_when(grovsortiment == "Sagtømmer" ~ "Sawlogs",
+                                    grovsortiment == "Massevirke" ~ "Pulpwood",
+                                    grovsortiment == "Vrak" ~ "Wrecked",
+                                    grovsortiment == "Energi" ~ "Energy",
+                                    TRUE ~ sortiment)
     )
   return(tbbl)
 }
